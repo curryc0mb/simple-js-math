eec6c6ff2238c3a6462a729e0fbd76b614b45476
curryc0mb <curryc0mb@users.noreply.github.com>
2016-07-28 23:12:50 -0500

    Update math.js

--- math.js
+++ math.js
@@ -1,40 +1,43 @@
-﻿// math.js, functions called by forms in the display.html file
+// math.js, functions called by forms in the display.html file
 
 function answerOne() {
     var a = document.forms["form-one"]["in-one"].value;
+	var b = document.getElementById("result-one").innerHTML;
     if (a == 4) {
-	  document.getElementById("result-one").innerHTML = "<b>Correct</b>";
+	  b = "<b>Correct</b>";
 	}
 	else if (a == "") {
-	  document.getElementById("result-one").innerHTML = "<b>Please enter a value</b>";
+	  b = "<b>Please enter a value</b>";
 	}
 	else {
-	  document.getElementById("result-one").innerHTML = "<b>Incorrect</b>";
-  }
+	  b = "<b>Incorrect</b>";
+        }
 }
 
 function answerTwo() {
     var a = document.forms["form-two"]["in-two"].value;
+	var b = document.getElementById("result-two").innerHTML;
 	if (a == 5) {
-	  document.getElementById("result-two").innerHTML = "<b>Correct</b>";
+	  b = "<b>Correct</b>";
 	}
 	else if (a == "") {
-	  document.getElementById("result-two").innerHTML = "<b>Please enter a value</b>";
+	  b = "<b>Please enter a value</b>";
 	}
 	else {
-	  document.getElementById("result-two").innerHTML = "<b>Incorrect</b>";
+	  b = "<b>Incorrect</b>";
 	}
 }
 
 function answerThree() {
     var a = document.forms["form-three"]["in-three"].value;
+	var b = document.getElementById("result-three").innerHTML;
 	if (a == 34.6) {
-	  document.getElementById("result-three").innerHTML = "<b>Correct</b>";
+	  b = "<b>Correct</b>";
 	}
 	else if (a == "") {
-	  document.getElementById("result-three").innerHTML = "<b>Please enter a value</b>";
+	  b = "<b>Please enter a value</b>";
 	}
 	else {
-	  document.getElementById("result-three").innerHTML = "<b>Incorrect</b>";
+	  b = "<b>Incorrect</b>";
 	}
 }
